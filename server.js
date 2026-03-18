da244772ed7a4eab00d2a9cc35ae4c836ae4173c
bbotransports-eng <bbotransports@gmail.com>
2026-03-18 13:50:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL || "http://localhost:5173",
-    /\.lovable\.app$/,
-    "http://localhost:3000",
-    "http://localhost:5173",
-  ],
-  credentials: true,
+  origin: "*",
+  credentials: false,
 }));
 
 app.use(express.json());
